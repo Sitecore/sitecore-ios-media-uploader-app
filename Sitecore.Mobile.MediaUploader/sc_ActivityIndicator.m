61157b270df2041507896bc5bcb3a503cbda9636
Igor <igor.kh.mail@gmail.com>
2014-04-14 15:10:01 +0300

    code style fix

--- Sitecore.Mobile.MediaUploader/sc_ActivityIndicator.m
+++ Sitecore.Mobile.MediaUploader/sc_ActivityIndicator.m
@@ -23,35 +23,38 @@
     if( !self ) return nil;
     
     self.hidden=YES;
-    self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5];
+    self.backgroundColor = [ UIColor colorWithRed:0 green:0 blue:0 alpha:0.5 ];
     self.clipsToBounds = YES;
     self.layer.cornerRadius = 10.0;
     
     self->_activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
     self->_activityView.frame = CGRectMake(65, 40, self->_activityView.bounds.size.width, self->_activityView.bounds.size.height);
-    [self addSubview:self->_activityView];
+    [ self addSubview: self->_activityView ];
     
     self->_loadingLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 115, 130, 22)];
     self->_loadingLabel.backgroundColor = [UIColor clearColor];
     self->_loadingLabel.textColor = [UIColor whiteColor];
     self->_loadingLabel.adjustsFontSizeToFitWidth = YES;
     self->_loadingLabel.textAlignment = NSTextAlignmentCenter;
-    [self addSubview:self->_loadingLabel];
-    [self->_activityView startAnimating];
+    [ self addSubview: self->_loadingLabel ];
+    [ self->_activityView startAnimating ];
     
     return self;
 }
 
 -(void) showWithLabel: (NSString *)label
 {
-    [self showWithLabel:label afterDelay:0];
+    [ self showWithLabel: label
+              afterDelay: 0 ];
 }
 
 -(void) showWithLabel: (NSString *)label afterDelay: (float) wait
 {
     [self cancelAllRequests];
     self->_loadingLabel.text = label;
-    [self performSelector:@selector(showActivityView) withObject:nil afterDelay:wait];
+    [ self performSelector: @selector(showActivityView)
+                withObject: nil
+                afterDelay: wait ];
 }
 
 -(void) showActivityView
@@ -62,12 +65,12 @@
 -(void) hide
 {
     [self cancelAllRequests];
-    self.hidden=YES;
+    self.hidden = YES;
 }
 
 -(void) cancelAllRequests
 {
-    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+    [ NSObject cancelPreviousPerformRequestsWithTarget: self ];
 }
 
 @end
