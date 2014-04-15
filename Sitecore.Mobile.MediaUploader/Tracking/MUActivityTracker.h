b79c0f3e7168f9dd0e2a65162234b2e1de27c924
dodikk <dodikk88.reg@gmail.com>
2014-04-15 09:49:20 +0300

    [tracking] Extracted a root header

---
+++ Sitecore.Mobile.MediaUploader/Tracking/MUActivityTracker.h
@@ -0,0 +1,13 @@
+#ifndef Sitecore_Mobile_MediaUploader_MUActivityTracker_h
+#define Sitecore_Mobile_MediaUploader_MUActivityTracker_h
+
+
+#import <Sitecore.Mobile.MediaUploader/Tracking/MUTrackable.h>
+#import <Sitecore.Mobile.MediaUploader/Tracking/MUSessionTracker.h>
+
+#import <Sitecore.Mobile.MediaUploader/Tracking/MUEventsTracker.h>
+#import <Sitecore.Mobile.MediaUploader/Tracking/MUEventsTrackerPOD.h>
+
+
+#endif
+
