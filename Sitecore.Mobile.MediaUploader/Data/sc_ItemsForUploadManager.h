910b5f0d95df7b6b31a09830391b8a73c546f2eb
Igor <igor.kh.mail@gmail.com>
2014-04-15 14:59:51 +0300

    new resources for uploading cells, uploading items manager moved to the separate class

---
+++ Sitecore.Mobile.MediaUploader/Data/sc_ItemsForUploadManager.h
@@ -0,0 +1,13 @@
+//
+//  sc_UploadItemManager.h
+//  Sitecore.Mobile.MediaUploader
+//
+//  Created by Igor on 14/04/14.
+//  Copyright (c) 2014 Sitecore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sc_UploadItemManager : NSObject
+
+@end
---
+++ Sitecore.Mobile.MediaUploader/Data/sc_ItemsForUploadManager.m
@@ -0,0 +1,13 @@
+//
+//  sc_UploadItemManager.m
+//  Sitecore.Mobile.MediaUploader
+//
+//  Created by Igor on 14/04/14.
+//  Copyright (c) 2014 Sitecore. All rights reserved.
+//
+
+#import "sc_UploadItemManager.h"
+
+@implementation sc_UploadItemManager
+
+@end
