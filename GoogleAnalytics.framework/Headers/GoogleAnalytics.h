0efd6508cc7debccaf764d5f7470c0b1030449ff
dodikk <dodikk88.reg@gmail.com>
2014-04-15 16:59:08 +0300

    [google analytics] Added a root header

---
+++ GoogleAnalytics.framework/Headers/GoogleAnalytics.h
@@ -0,0 +1,8 @@
+
+#import <GoogleAnalytics/GAI.h>
+#import <GoogleAnalytics/GAILogger.h>
+#import <GoogleAnalytics/GAITracker.h>
+#import <GoogleAnalytics/GAITrackedViewController.h>
+
+#import <GoogleAnalytics/GAIDictionaryBuilder.h>
+#import <GoogleAnalytics/GAIFields.h>
