2ad4059a631c62e237a87686f3e0a40d2b66e1e2
dodikk <dodikk88.reg@gmail.com>
2014-04-16 11:41:19 +0300

    [lib] Added an "aggregate library" project

---
+++ lib/MUAllUploaderLibs/MUAllUploaderLibs/MUAllUploaderLibs.h
@@ -0,0 +1,13 @@
+//
+//  MUAllUploaderLibs.h
+//  MUAllUploaderLibs
+//
+//  Created by Oleksandr Dodatko on 4/16/14.
+//  Copyright (c) 2014 Sitecore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MUAllUploaderLibs : NSObject
+
+@end
