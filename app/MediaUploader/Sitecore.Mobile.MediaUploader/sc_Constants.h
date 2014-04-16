49986eeae59c074a5747463cda6ef72bf4daddd3
dodikk <dodikk88.reg@gmail.com>
2014-04-16 14:40:55 +0300

    [uploader] using OBJC_EXTERN keyword

--- app/MediaUploader/Sitecore.Mobile.MediaUploader/sc_Constants.h
+++ app/MediaUploader/Sitecore.Mobile.MediaUploader/sc_Constants.h
@@ -9,19 +9,21 @@
 #ifndef Sitecore_Mobile_MediaUploader_sc_Constants_h
 #define Sitecore_Mobile_MediaUploader_sc_Constants_h
 
-enum {
- UPLODIMAGESIZE_SMALL = 0,
- UPLODIMAGESIZE_MEDIUM = 1,
- UPLODIMAGESIZE_ACTUAL = 2
+#import <Foundation/Foundation.h>
+
+enum
+{
+    UPLODIMAGESIZE_SMALL  = 0,
+    UPLODIMAGESIZE_MEDIUM = 1,
+    UPLODIMAGESIZE_ACTUAL = 2
 };
 
-extern NSString* const VIDEO_TEMPLATE_PATH     ;
-extern NSString* const IMAGE_TEMPLATE_PATH     ;
-extern NSString* const IMAGE_JPEG_TEMPLATE_PATH;
-extern NSString* const MEDIA_FOLDER_PATH       ;
-extern NSString* const ITEM_TEMPLATE_PATH      ;
+OBJC_EXTERN NSString* const VIDEO_TEMPLATE_PATH     ;
+OBJC_EXTERN NSString* const IMAGE_TEMPLATE_PATH     ;
+OBJC_EXTERN NSString* const IMAGE_JPEG_TEMPLATE_PATH;
+OBJC_EXTERN NSString* const MEDIA_FOLDER_PATH       ;
+OBJC_EXTERN NSString* const ITEM_TEMPLATE_PATH      ;
 
-#endif
 
 @interface sc_Constants : NSObject
 
@@ -29,3 +31,6 @@ extern NSString* const ITEM_TEMPLATE_PATH      ;
 +(CGSize)smallImageSize;
 
 @end
+
+
+#endif
