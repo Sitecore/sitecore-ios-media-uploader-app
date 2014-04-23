694cce677a720d0b4718e88db6936a894f3bc25a
Igor <igor.kh.mail@gmail.com>
2014-04-23 17:30:36 +0300

    prefixes changed for classes from upload management library

--- lib/MUUploadManager/MUUploadManager/sc_ImageScaling.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-
-@interface sc_ImageScaling : NSObject
-
-+(UIImage*)scaleImage:(UIImage*)image toSize:(CGSize)size;
-+(CGFloat)scaleCoefficientToResizeImageWithSize:(CGSize)originalSize toSize:(CGSize)resultSize;
-
-@end
