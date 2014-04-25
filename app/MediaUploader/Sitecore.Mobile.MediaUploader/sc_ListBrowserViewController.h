90c6a8ef6c315a7feb21a41e1bb39e1ec8802d51
dodikk <dodikk88.reg@gmail.com>
2014-04-25 12:24:49 +0300

    [GUI] Cleaning up item path text view placeholder

--- app/MediaUploader/Sitecore.Mobile.MediaUploader/sc_ListBrowserViewController.h
+++ app/MediaUploader/Sitecore.Mobile.MediaUploader/sc_ListBrowserViewController.h
@@ -5,6 +5,7 @@ typedef void (^SCUPloadFolderReceived)(NSString* folder);
 
 @interface sc_ListBrowserViewController : UIViewController
 
--(void)chooseUploaderFolderForSite:(SCSite*)site witCallback:(SCUPloadFolderReceived)callback;
+-(void)chooseUploaderFolderForSite:(SCSite*)site
+                       witCallback:(SCUPloadFolderReceived)callback;
 
 @end
