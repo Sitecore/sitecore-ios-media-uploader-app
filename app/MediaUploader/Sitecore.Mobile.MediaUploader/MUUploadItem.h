15f64dda48c4ae6a98d47832028af0d4bcc2d9b0
Igor <igor.kh.mail@gmail.com>
2014-04-25 18:36:29 +0300

    garbage class removed

---
+++ app/MediaUploader/Sitecore.Mobile.MediaUploader/MUUploadItem.h
@@ -0,0 +1,28 @@
+//
+//  MUUploadItem.h
+//  Sitecore.Mobile.MediaUploader
+//
+//  Created by andrea bellagamba on 7/30/13.
+//  Copyright (c) 2013 Sitecore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MUMedia.h"
+
+
+@interface MUUploadItem: NSObject
+
+@property (nonatomic) MUMedia* mediaItem;
+@property (nonatomic) NSData* data;
+
+-(id)initWithObjectData:(MUMedia*)mediaItem
+                   data:(NSData*)data;
+
+@property (nonatomic, readonly) BOOL isImage;
+@property (nonatomic, readonly) BOOL isVideo;
+@property (nonatomic, readonly) NSString* fileName;
+@property (nonatomic, readonly) NSString* itemTemplate;
+@property (nonatomic, readonly) NSString* contentType;
+@property (nonatomic, readonly) NSURL* assetURL;
+
+@end
