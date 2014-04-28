1969f50e8b149f8f50d0fa8ebeeded24fa44fab7
Igor <igor.kh.mail@gmail.com>
2014-04-28 14:15:36 +0300

    location data moved to separate class

---
+++ lib/MUUploadManager/MUUploadManager/MULocationInfoData.h
@@ -0,0 +1,13 @@
+//
+//  MULocationInfo.h
+//  MUUploadManager
+//
+//  Created by Igor on 28/04/14.
+//  Copyright (c) 2014 Sitecore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MULocationInfo : NSObject
+
+@end
---
+++ lib/MUUploadManager/MUUploadManager/MULocationInfoData.m
@@ -0,0 +1,13 @@
+//
+//  MULocationInfo.m
+//  MUUploadManager
+//
+//  Created by Igor on 28/04/14.
+//  Copyright (c) 2014 Sitecore. All rights reserved.
+//
+
+#import "MULocationInfo.h"
+
+@implementation MULocationInfo
+
+@end
