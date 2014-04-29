147d3c4bd2e29d38d7759c5e252fa5bce85acdba
Igor <igor.kh.mail@gmail.com>
2014-04-29 17:03:21 +0300

    classes renamed

---
+++ lib/MUUploadManager/MUUploadManager/MUTypes.h
@@ -0,0 +1,14 @@
+//
+//  MUTypes.h
+//  MUUploadManager
+//
+//  Created by Igor on 29/04/14.
+//  Copyright (c) 2014 Sitecore. All rights reserved.
+//
+
+#ifndef MUUploadManager_MUTypes_h
+#define MUUploadManager_MUTypes_h
+
+
+
+#endif
