3ab6e47231370988052c0c86e445c677d499786c
dodikk <dodikk88.reg@gmail.com>
2014-04-29 12:24:45 +0300

    [CI] Added helper scripts

---
+++ scripts/simulator/CleanTestReports.sh
@@ -0,0 +1,10 @@
+## TEMP_DIR=$(/usr/bin/getconf DARWIN_USER_TEMP_DIR)
+## TEST_DIR_NAME=test-results
+
+## TEST_RESULTS_DIR=$TEMP_DIR$TEST_DIR_NAME
+
+## rm -r -f "$TEST_RESULTS_DIR"
+
+cd ~/Library/Application\ Support/iPhone\ Simulator/7.1/
+rm -rf Applications
+mkdir -p Applications
---
+++ scripts/simulator/CopyTestReports.sh
@@ -0,0 +1,36 @@
+LAUNCH_DIR=$PWD
+
+cd ../../
+   PROJECT_ROOT=$PWD
+   
+   if [ ! -d "deployment" ]; then   
+      mkdir deployment
+   fi
+
+   cd deployment
+      DEPLOYMENT_DIR=$PWD
+cd "$LAUNCH_DIR"
+
+TEST_PUBLISH_DIR=$DEPLOYMENT_DIR/test-results
+
+
+rm -r -f "$TEST_PUBLISH_DIR"
+mkdir -p "$TEST_PUBLISH_DIR"
+
+
+cd ~/Library/Application\ Support/iPhone\ Simulator/7.1/Applications
+for directory in $( ls -1 ); do
+   echo "$directory/tmp/test-results"
+   ls -1 "$directory/tmp/test-results"
+
+   for report in $( ls -1 "$directory/tmp/test-results/" ); do
+       cp "$directory/tmp/test-results/$report" "$TEST_PUBLISH_DIR"
+   done
+
+   echo "============================"
+done
+cd "$LAUNCH_DIR"
+
+cd "$DEPLOYMENT_DIR"
+   zip -r test-results.zip test-results
+cd "$LAUNCH_DIR"
