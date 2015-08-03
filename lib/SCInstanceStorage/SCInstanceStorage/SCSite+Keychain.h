62c153fefd55f31f8dfc6bc121a31ac216b31811
Igor <igor.kh.mail@gmail.com>
2015-08-03 17:32:38 +0300

    resolving conflicts

---
+++ lib/SCInstanceStorage/SCInstanceStorage/SCSite+Keychain.h
@@ -0,0 +1,13 @@
+//
+//  SCSite+Keychain.h
+//  SCInstanceStorage
+//
+//  Created by Oleksandr Dodatko on 5/13/14.
+//  Copyright (c) 2014 Sitecore. All rights reserved.
+//
+
+#import "SCSite.h"
+
+@interface SCSite (Keychain)
+
+@end
